87ed416f15d317068188c03a3225e93427661155
longma <longma@longmas-MBP.sv.cmu.local>
2015-02-09 16:07:54 -0800

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Penguin.m
@@ -0,0 +1,21 @@
+//
+//  Penguin.m
+//  PeevedPenguins
+//
+//  Created by longma on 2/9/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "Penguin.h"
+
+@implementation Penguin
+- (id)init {
+    self = [super init];
+    
+    if (self) {
+        CCLOG(@"Penguin created");
+    }
+    
+    return self;
+}
+@end
