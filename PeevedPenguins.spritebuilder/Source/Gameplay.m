cb48837d7341230bb61b3c1a36db8ea6367697af
longma <longma@longmas-MBP.sv.cmu.local>
2015-02-09 17:58:45 -0800

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -103,6 +103,11 @@
     }
 }
 - (void)sealRemoved:(CCNode *)seal {
+    CCParticleSystem *explosion = (CCParticleSystem *)[CCBReader load:@"SealExplosion"];
+    explosion.autoRemoveOnFinish = TRUE;
+    explosion.position = seal.position;
+    [seal.parent addChild:explosion];
+    
     [seal removeFromParent];
 }
 @end
